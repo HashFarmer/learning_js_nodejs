b2ba21a3769204fbee1adf5dda5873c662046998
coder <123456@qq.com>
2022-01-22 23:46:52 +0800

    first commit

---
+++ hell_setTimeout.js
@@ -0,0 +1,14 @@
+//outer -> inner
+
+setTimeout(()=>{
+    console.log('preparing x value.');
+    let x = 3;
+    setTimeout(()=>{
+        console.log('preparing y value.');
+        let y = 4;
+        setTimeout(()=>{
+            console.log('calculating x + y.');
+            console.log(x + y);
+        },1000);
+    },1000);
+},1000);
