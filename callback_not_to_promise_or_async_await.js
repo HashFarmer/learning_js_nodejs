5de28d75a334a78a4004b0849b32da97ca6b626c
unknown <123456@qq.com>
2022-03-22 21:25:16 +0800

    xx

--- callback_not_to_promise_or_async_await.js
+++ callback_not_to_promise_or_async_await.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 console.log("ab");
-fs.readFile("1.txt").then(()=>{console.log("fs")};);
+//必须加promises，否则不能连接then结构
+fs.promises.readFile("1.txt").then(()=>{console.log("fs")});
 //fs.readFile("1.txt",(err,data)=>{if(err){console.log("error");}else{console.log(data.toString());}})
 console.log("cd");
